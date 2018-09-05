c05ac838f6bd06ae69ddade3d33bfdcd0c9767c3
VE3NEA <alshovk@dxatlas.com>
2018-09-05 12:54:37 -0400

    dissect UDP item header

--- afedri.lua
+++ afedri.lua
@@ -22,10 +22,10 @@ function afedri_udp_protocol.dissector(buffer, pinfo, tree)
     if buffer:len() < 4 then return end
     pinfo.cols.protocol = afedri_udp_protocol.name
     subtree = tree:add(afedri_udp_protocol, buffer(), "Afedri Protocol Data")
-    Word.dissect(buffer(0, 2), "signature")
+    UdpHeader.dissect(buffer(0, 2))
     Word.dissect(buffer(2, 2), "sequence number")
     IqSamples.dissect(buffer(4, buffer:len()-4))
-end  
+end
 
 function afedri_tcp_protocol.dissector(buffer, pinfo, tree)
   if buffer:len() < 4 then return end
@@ -54,61 +54,61 @@ end
 function dissect_net_message(buffer)
     local control_item = ControlItem.dissect(buffer(2,2))
 
-    if control_item == CI_FREQUENCY then 
+    if control_item == CI_FREQUENCY then
         Channel.dissect(buffer(4,1))
         Frequency.dissect(buffer(5,4))
 
-    elseif control_item == CI_RECEIVER_STATE then 
+    elseif control_item == CI_RECEIVER_STATE then
         ReceiverState.dissect(buffer(5,1))
 
-    elseif control_item == CI_RF_GAIN then 
-        Channel.dissect(buffer(4,1)) 
+    elseif control_item == CI_RF_GAIN then
+        Channel.dissect(buffer(4,1))
         Gain.dissect(buffer(5,1))
 
-    elseif control_item == CI_DDC_SAMPLE_RATE then 
+    elseif control_item == CI_DDC_SAMPLE_RATE then
         Frequency.dissect(buffer(5,4), "sampling rate")
 
-    elseif control_item == CI_SAMPLE_RATE_CALIBRATION then 
+    elseif control_item == CI_SAMPLE_RATE_CALIBRATION then
         -- if len=4, no args
-        if buffer:len() == 9 then Frequency.dissect(buffer(5,4), "clock rate") end 
+        if buffer:len() == 9 then Frequency.dissect(buffer(5,4), "clock rate") end
 
-    elseif control_item == CI_DATA_PACKET_SIZE then 
+    elseif control_item == CI_DATA_PACKET_SIZE then
         PacketSize.dissect(buffer(4,1))
 
-    else 
+    else
         RawData.dissect(buffer(4, buffer:len()-4))
     end
 end
 
 function dissect_hid_message(buffer)
-    local hid_command_type = HidCommandType.dissect(buffer(2,1))    
+    local hid_command_type = HidCommandType.dissect(buffer(2,1))
 
     if (hid_command_type == HID_FREQUENCY_REPORT) or (hid_command_type == HID_FREQUENCY_REPLY) then
         Frequency.dissect(buffer(3,4))
         Channel.dissect(buffer(7,1))
 
-    elseif  hid_command_type == HID_GENERIC_REPORT 
+    elseif  hid_command_type == HID_GENERIC_REPORT
       then dissect_hid_request(buffer, subtree)
 
-    elseif  hid_command_type == HID_GENERIC_REPLY 
+    elseif  hid_command_type == HID_GENERIC_REPLY
       then dissect_hid_reply(buffer)
     end
 end
 
 function dissect_hid_request(buffer)
-    local hid_command = HidCommand.dissect(buffer(3,1))    
+    local hid_command = HidCommand.dissect(buffer(3,1))
     local cmd = HidCommands[hid_command]
     local start_pos = 4
 
     if cmd ~= nil then
-        if cmd.arg1 ~= nil then 
+        if cmd.arg1 ~= nil then
             cmd.arg1.type.dissect(buffer(start_pos, cmd.arg1.type.length), cmd.arg1.label)
             start_pos = start_pos + cmd.arg1.type.length
         end
-        if cmd.arg2 ~= nil then 
+        if cmd.arg2 ~= nil then
             cmd.arg2.type.dissect(buffer(start_pos, cmd.arg2.type.length), cmd.arg2.label)
         end
-    else        
+    else
         RawData.dissect(buffer(start_pos, buffer:len()-start_pos))
     end
 end
@@ -154,6 +154,7 @@ Word = {length = 2}
 DWord = {length = 4}
 RawData = {length = 5}
 IqSamples = {length = 4}
+UdpHeader = {length = 2}
 
 
 --constants
@@ -193,28 +194,35 @@ HidCommandTypes = {
 }
 
 ControlItems = {
-    [0x0001] = "CI_TARGET_NAME",     
-    [0x0002] = "CI_TARGET_SERIAL_NUMBER",     
-    [0x0003] = "CI_INTERFACE_VERSION",     
-    [0x0004] = "CI_HW_FW_VERSION",     
-    [0x0005] = "CI_STATUS_ERROR_CODE",     
-    [0x0009] = "CI_PRODUCT_ID",     
+    [0x0001] = "CI_TARGET_NAME",
+    [0x0002] = "CI_TARGET_SERIAL_NUMBER",
+    [0x0003] = "CI_INTERFACE_VERSION",
+    [0x0004] = "CI_HW_FW_VERSION",
+    [0x0005] = "CI_STATUS_ERROR_CODE",
+    [0x0009] = "CI_PRODUCT_ID",
     [0x0018] = "CI_RECEIVER_STATE",
-    [0x0020] = "CI_FREQUENCY",          
-    [0x0038] = "CI_RF_GAIN",        
-    [0x0048] = "CI_AF_GAIN",     
-    [0x0044] = "CI_RF_FILTER_SELECT",     
-    [0x008A] = "CI_AD_MODES",     
-    [0x00B4] = "CI_INPUT_SYNC_MODES",    
-    [0x00B8] = "CI_DDC_SAMPLE_RATE",    
-    [0x00B0] = "CI_SAMPLE_RATE_CALIBRATION",  --read clock frequency   
-    [0x00D0] = "CI_CALIBRATION_DATA",     
-    [0x00B6] = "CI_PULSE_OUTPUT_MODE",     
-    [0x012A] = "CI_DA_OUTPUT_MODE",     
-    [0x00C4] = "CI_DATA_PACKET_SIZE",   
-    [0x00C5] = "CI_UDP_IP_PORT",     
-    [0x0200] = "CI_RS232_OPEN",     
+    [0x0020] = "CI_FREQUENCY",
+    [0x0038] = "CI_RF_GAIN",
+    [0x0048] = "CI_AF_GAIN",
+    [0x0044] = "CI_RF_FILTER_SELECT",
+    [0x008A] = "CI_AD_MODES",
+    [0x00B4] = "CI_INPUT_SYNC_MODES",
+    [0x00B8] = "CI_DDC_SAMPLE_RATE",
+    [0x00B0] = "CI_SAMPLE_RATE_CALIBRATION",  --read clock frequency
+    [0x00D0] = "CI_CALIBRATION_DATA",
+    [0x00B6] = "CI_PULSE_OUTPUT_MODE",
+    [0x012A] = "CI_DA_OUTPUT_MODE",
+    [0x00C4] = "CI_DATA_PACKET_SIZE",
+    [0x00C5] = "CI_UDP_IP_PORT",
+    [0x0200] = "CI_RS232_OPEN",
     [0x0201] = "CI_RS232_CLOSE"
+}
+
+UdpHeaders = {
+    [0x8404] = "16-Bit data, large packet",
+    [0x8204] = "16-Bit data, small packet",
+    [0x85A4] = "24-Bit data, large packet",
+    [0x8484] = "24-Bit data, small packet"
 }
 
 OnOffValues = {
@@ -224,8 +232,8 @@ OnOffValues = {
 
 Channels = {
     [0] = "AFEDRI_CH1",
-    [2] = "AFEDRI_CH2", 
-    [3] = "AFEDRI_CH3", 
+    [2] = "AFEDRI_CH2",
+    [3] = "AFEDRI_CH3",
     [4] = "AFEDRI_CH4"
 }
 
@@ -235,13 +243,13 @@ MultiChannelModes = {
     [2] = "DUAL_CHANNEL_MODE",
     [3] = "DIVERSITY_INTERNAL_ADD_MODE",
     [4] = "QUAD_DIVERSITY_MODE",
-    [5] = "QUAD_CHANNEL_MODE"   
+    [5] = "QUAD_CHANNEL_MODE"
 }
 
 SdrModels = {
     [0] = "'AFEDRI SDR-Net'",
     [1] = "'SDR-IP'",
-    [2] = "'AFE822x SDR-Net'" 
+    [2] = "'AFE822x SDR-Net'"
 }
 
 PacketSizes = {
@@ -259,7 +267,7 @@ ReceiverStates = {
 
 HidCommands = {
     --only as return value in ACK message
-    [3] = {name = "HID_FREQUENCY_REPORT"}, 
+    [3] = {name = "HID_FREQUENCY_REPORT"},
     --only SDR to PC
     [27] = {name = "HID_GENERIC_OVERLAOD_STATE"}, -- bug in mainwindow.cpp, return type unknown
     [28] = {name = "HID_GENERIC_ACK", ret = {type = HidCommand, label = "reply to"}},
@@ -312,13 +320,13 @@ HidCommands = {
     [0x55] = {name = "HID_READ_EEPROM_COMMAND", arg1 = {type = Byte, label = "address"}, ret = {type = Word, label = "EEPROM data"}},
     [68] = {name = "HID_GENERIC_SET_USB_MODE", arg1 = {type = Byte, label = "mode"}},
     [69] = {name = "HID_GENERIC_SET_OVR_MODE", arg1 = {type = Bits, label = "AGC on/off"}}, --todo: bit field
-    [70] = {name = "HID_GENERIC_AGC_LEVEL_COMMAND", arg1 = {type = Channel}}, 
+    [70] = {name = "HID_GENERIC_AGC_LEVEL_COMMAND", arg1 = {type = Channel}},
     [79] = {name = "HID_GENERIC_SET_R820T_LNA_GAIN_COMMAND", arg1 = {type = Byte, label = "gain"}},  -- find out the format
     [80] = {name = "HID_GENERIC_SET_R820T_MIXER_GAIN_COMMAND", arg1 = {type = Byte, label = "gain"}},  -- find out the format
     [81] = {name = "HID_GENERIC_SET_R820T_VGA_GAIN_COMMAND", arg1 = {type = Byte, label = "gain"}},  -- find out the format
-    [84] = {name = "HID_GENERIC_SET_R820T_IF_BW_COMMAND", arg1 = {type = Byte, label = "bandwidth"}}, 
-    [88] = {name = "HID_GENERIC_SET_R820T_HIGHPASS_BW_COMMAND", arg1 = {type = Byte, label = "low cutoff"}}, 
-    [89] = {name = "HID_GENERIC_SET_R820T_LOWPASS_BW_COMMAND", arg1 = {type = Byte, label = "high cutoff"}}, 
+    [84] = {name = "HID_GENERIC_SET_R820T_IF_BW_COMMAND", arg1 = {type = Byte, label = "bandwidth"}},
+    [88] = {name = "HID_GENERIC_SET_R820T_HIGHPASS_BW_COMMAND", arg1 = {type = Byte, label = "low cutoff"}},
+    [89] = {name = "HID_GENERIC_SET_R820T_LOWPASS_BW_COMMAND", arg1 = {type = Byte, label = "high cutoff"}},
     --byte + byte
     [2] = {name = "HID_GENERIC_GAIN_COMMAND", arg1 = {type = Byte, label = "gain"}, arg2 = {type = Channel}}, -- for AFE822x should be 0
     [8] = {name = "HID_GENERIC_DAC_COMMAND", arg1 = {type = Gain}, arg2 = {type = Channel}}, -- rf gain, maps to dB
@@ -330,20 +338,20 @@ HidCommands = {
     --byte + word
     [0x56] = {name = "HID_WRITE_EEPROM_COMMAND", arg1 = {type = Byte, label = "address"}, arg2 = {type = Word, label = "data"}},
     --word + byte
-    [56] = {name = "HID_GENERIC_SET_PHASE_SHIFT_COMMAND", arg1 = {type = Word, label = "phase"}, arg2 = {type = Channel}},    
+    [56] = {name = "HID_GENERIC_SET_PHASE_SHIFT_COMMAND", arg1 = {type = Word, label = "phase"}, arg2 = {type = Channel}},
     --word + word
     [39] = {name = "HID_GENERIC_WRITE_HWORD_TO_FLASH", arg1 = {type = Word, label = "address"}, arg2 = {type = Word, label = "data"}},
     --ip
-    [21] = {name = "HID_GENERIC_SAVE_SDR_IP_ADDRESS", arg1 = {type = IpAddress, label = "IP address"}}, 
-    [22] = {name = "HID_GENERIC_SAVE_SDR_IP_MASK", arg1 = {type = IpAddress, label = "IP mask"}}, 
-    [23] = {name = "HID_GENERIC_SAVE_GATEWAY_IP_ADDRESS", arg1 = {type = IpAddress, label = "IP gateway"}}, 
-    [24] = {name = "HID_GENERIC_SAVE_DST_IP_ADDRESS", arg1 = {type = Byte, IpAddress = "dst IP address"}}, 
+    [21] = {name = "HID_GENERIC_SAVE_SDR_IP_ADDRESS", arg1 = {type = IpAddress, label = "IP address"}},
+    [22] = {name = "HID_GENERIC_SAVE_SDR_IP_MASK", arg1 = {type = IpAddress, label = "IP mask"}},
+    [23] = {name = "HID_GENERIC_SAVE_GATEWAY_IP_ADDRESS", arg1 = {type = IpAddress, label = "IP gateway"}},
+    [24] = {name = "HID_GENERIC_SAVE_DST_IP_ADDRESS", arg1 = {type = Byte, IpAddress = "dst IP address"}},
     --dword
     [25] = {name = "HID_GENERIC_SAVE_DST_PORT", arg1 = {type = Word, label = "port"}}, --dword in mainwindow.cpp, should be word
-    [92] = {name = "HID_GENERIC_SAVE_MAC_COMMAND", arg1 = {type = MacAddress}, ret = {type = MacAddress}}, 
-    [30] = {name = "HID_GENERIC_SET_SAMPLE_RATE_COMMAND", arg1 = {type = Frequency, label = "sampling rate"}}, 
-    [78] = {name = "HID_GENERIC_SET_R820T_FREQ_COMMAND", arg1 = {type = Frequency, label = "frequency"}}, 
-    [90] = {name = "HID_GENERIC_SAVE_R820T_REF_FREQ_COMMAND", arg1 = {type = Frequency, label = "frequency"}}, 
+    [92] = {name = "HID_GENERIC_SAVE_MAC_COMMAND", arg1 = {type = MacAddress}, ret = {type = MacAddress}},
+    [30] = {name = "HID_GENERIC_SET_SAMPLE_RATE_COMMAND", arg1 = {type = Frequency, label = "sampling rate"}},
+    [78] = {name = "HID_GENERIC_SET_R820T_FREQ_COMMAND", arg1 = {type = Frequency, label = "frequency"}},
+    [90] = {name = "HID_GENERIC_SAVE_R820T_REF_FREQ_COMMAND", arg1 = {type = Frequency, label = "frequency"}},
     --inverted command
     [4] = {name = "HID_GENERIC_GET_INIT_STATUS_COMMAND", arg1 = {type = Byte, label = "inverted HID command"}, ret = {type = InitStatus}},
     [0xE1] = {name = "HID_GENERIC_INIT_FE_COMMAND", arg1 = {type = Byte, label = "inverted HID command"}},
@@ -370,7 +378,7 @@ function Gain.dissect(data, label)
     local byte = data:le_int()
     local gain = byte
     if gain > 0 then gain = gain * 3/8 - 10 end
-    subtree:add(data, label .. ": " .. byte .. " (" .. gain .. " dB)")    
+    subtree:add(data, label .. ": " .. byte .. " (" .. gain .. " dB)")
 end
 
 function Frequency.dissect(data, label)
@@ -382,7 +390,7 @@ function IpAddress.dissect(data, label)
     if label == nil then label = "IP address" end
     local dword = data:le_uint()
     ip = data(3,1):uint() .. "." .. data(2,1):uint() .. "." .. data(1,1):uint() .. "." .. data(0,1):uint();
-    subtree:add(data, label .. ": " .. ip)    
+    subtree:add(data, label .. ": " .. ip)
 end
 
 
@@ -391,7 +399,7 @@ function MacAddress.dissect(data, label)
     local byte1 = data(0,1):le_uint()
     local byte2 = data(1,1):le_uint()
     mac = string.format("0x%04X (82:2A:%02X:%02X:%02X:%02X)", data:le_uint(), byte2, byte1, byte2, byte1)
-    subtree:add(data, label .. ": " .. mac)    
+    subtree:add(data, label .. ": " .. mac)
 end
 
 function InitStatus.dissect(data, label)
@@ -399,7 +407,7 @@ function InitStatus.dissect(data, label)
     local value = data:le_uint()
     local str = "error"
     if value == 0 then str = "success" end
-    subtree:add(data, label .. ": " .. value .. " (" .. str .. ")")    
+    subtree:add(data, label .. ": " .. value .. " (" .. str .. ")")
 end
 
 function ControlItem.dissect(data, label)
@@ -443,31 +451,31 @@ function Bits.dissect(data, label)
     local value = data:le_uint()
     local str = ""
     for i=data:len() * 8 - 1, 0, -1 do str = str .. string.char(0x30 + bit32.extract(value, i, 1)) .. " " end
-    subtree:add(data, label .. ": " .. str)  
+    subtree:add(data, label .. ": " .. str)
 end
 
 function Char.dissect(data, label)
     if label == nil then label = "char" end
     local byte = data:le_uint()
-    subtree:add(data, label .. ": " .. string.format('%02X', byte) .. " ('" .. string.char(byte) .. "')")    
+    subtree:add(data, label .. ": " .. string.format('%02X', byte) .. " ('" .. string.char(byte) .. "')")
 end
 
 function Byte.dissect(data, label)
     if label == nil then label = "8-bit value" end
     local byte = data:le_uint()
-    subtree:add(data, label .. ": " .. byte .. " (0x" .. string.format('%02X', byte) .. ")")    
+    subtree:add(data, label .. ": " .. byte .. " (0x" .. string.format('%02X', byte) .. ")")
 end
 
 function Word.dissect(data, label)
     if label == nil then label = "16-bit value" end
     local word = data:le_uint()
-    subtree:add(data, label .. ": " .. word .. " (0x" .. string.format('%04X', word) .. ")")    
+    subtree:add(data, label .. ": " .. word .. " (0x" .. string.format('%04X', word) .. ")")
 end
 
 function DWord.dissect(data, label)
     if label == nil then label = "32-bit value" end
     local dword = data:le_uint()
-    subtree:add(data, label .. ": " .. dword .. " (0x" .. string.format('%08X', dword) .. ")")    
+    subtree:add(data, label .. ": " .. dword .. " (0x" .. string.format('%08X', dword) .. ")")
 end
 
 function RawData.dissect(data, label)
@@ -483,39 +491,35 @@ end
 
 function IqSamples.dissect(data, label)
     local byte_count = data:len()
-    if byte_count < 4 then return end    
+    if byte_count < 4 then return end
     if label == nil then label = "I/Q data" end
     local str = "   "
 
-    if (byte_count == 1028-4) or (byte_count == 515-4) then width = 2 
+    if (byte_count == 1028-4) or (byte_count == 515-4) then width = 2
     elseif (byte_count == 1444-4) or (byte_count == 388-4) then width = 3 end
 
     local sample_count = math.floor(byte_count / (2 * width))
     local node = subtree:add(data, label .. string.format(", %d 2x%d-bit samples", sample_count, width * 8))
 
-    for i = 0, sample_count-1 do 
+    for i = 0, sample_count-1 do
         str = str .. "(" .. sample(data(i*2*width, width)) .. ", " .. sample(data(i*2*width+width, width)) .. ")"
 
-        if (i % 4) == 3 then 
-            node:add(data((i-3)*2*width, 8 * width), str) 
-            str = "   " 
-            if i == 15 then node:add(data((i+1)*2*width, (sample_count-i-1) * 2 * width), "   ..."); break end 
+        if (i % 4) == 3 then
+            node:add(data((i-3)*2*width, 8 * width), str)
+            str = "   "
+            if i == 15 then node:add(data((i+1)*2*width, (sample_count-i-1) * 2 * width), "   ..."); break end
         end
     end
 
     if str ~= "   " then  node:add(data, str) end
 end
 
-function sample(data)
-    local v = data:le_int()
-
-    --16-bit, dec format
-    if data:len() == 2 then return string.format('%6d', v) end
-
-    --24-bit, hex format, helps to see zero padding
-    local str = string.format('+0x%06x', v)
-    if bit.band(v, 0x800000) ~= 0 then  str = string.format('-0x%06x', 0x1000000 - v) end
-    return str
+function UdpHeader.dissect(data, label)
+    if label == nil then label = "header" end
+    local value = data:le_uint()
+    local name = look_up(value, UdpHeaders)
+    subtree:add(data, label .. ": " .. string.format('0x%04X', value) .. " (" .. name .. ")")
+    return name ~= "Unknown"
 end
 
 
@@ -535,7 +539,7 @@ function dissect_lookup(data, table, label, default_label)
     if label == nil then label = default_label end
     local value = data:le_uint()
     local name = look_up(value, table)
-    subtree:add(data, label .. ": " .. value .. " (" .. name .. ")")    
+    subtree:add(data, label .. ": " .. value .. " (" .. name .. ")")
     return value
 end
 
@@ -543,12 +547,25 @@ end
 function comma_value(value)
     local formatted = value
     local k = 0
-    while true do  
+    while true do
       formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
       if k == 0 then break end
     end
     return formatted
   end
+
+function sample(data)
+    local v = data:le_int()
+
+    --16-bit, dec format
+    if data:len() == 2 then return string.format('%6d', v) end
+
+    --24-bit, hex format, helps to see zero padding
+    local str = string.format('+0x%06x', v)
+    if bit.band(v, 0x800000) ~= 0 then  str = string.format('-0x%06x', 0x1000000 - v) end
+    return str
+end
+
 
 
 
